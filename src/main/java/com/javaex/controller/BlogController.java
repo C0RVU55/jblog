6b49741ccda3ead908d1c26c3902ed042c6a6a1a
이다현 <74286308+C0RVU55@users.noreply.github.com>
2021-02-21 00:42:14 +0900

    blog admin

--- src/main/java/com/javaex/controller/BlogController.java
+++ src/main/java/com/javaex/controller/BlogController.java
@@ -1,5 +1,7 @@
 package com.javaex.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.javaex.service.BlogService;
 import com.javaex.vo.BlogVo;
+import com.javaex.vo.UserVo;
 
 @Controller
 public class BlogController {
@@ -18,7 +21,8 @@ public class BlogController {
 	
 	/*
 	1.로그인 다이어그램
-	2.내블로그 관리	베이직, 카테고리, 글작성
+	2.메인페이지 회색선 없애기
+	3.내블로그 관리	베이직, 카테고리(ajax), 글작성
 	*/
 	
 	//블로그 메인
@@ -34,6 +38,22 @@ public class BlogController {
 		return "blog/blog-main";
 	}
 	
-	//내블로그 관리
+	//내블로그 관리1 베이직
+	@RequestMapping(value= "{id}/admin/basic", method= {RequestMethod.GET, RequestMethod.POST})
+	public String adminBasic(@PathVariable("id") String id, Model model, HttpSession session) {
+		System.out.println("[BlogController.blogMain()] --> ");
+		
+		//세션 넣어서 본인 아니면 안 뜨게 하기(userNo 안 쓸 건데 넣는 게 맞는지)
+		int userNo = ((UserVo)session.getAttribute("authUser")).getUserNo();
+		
+		//블로그 정보 select 해오기
+		BlogVo blogVo = blogService.blogInfo(id);
+		System.out.println("[BlogController.blogMain()] --> "+blogVo);
+		model.addAttribute("blogVo", blogVo);
+		
+		return "blog/admin/blog-admin-basic";
+	}
+	
+	//내블로그 관리1 베이직 - 설정 변경
 	
 }
--- webapp/WEB-INF/views/blog/admin/blog-admin-basic.jsp
+++ webapp/WEB-INF/views/blog/admin/blog-admin-basic.jsp
@@ -1,5 +1,7 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -14,7 +16,7 @@
 	<div id="wrap">
 		
 		<!-- 개인블로그 해더 -->
-
+		<c:import url="/WEB-INF/views/includes/blog-header.jsp"></c:import>
 
 		<div id="content">
 			<ul id="admin-menu" class="clearfix">
@@ -33,7 +35,7 @@
 						</colgroup>
 			      		<tr>
 			      			<td><label for="textTitle">블로그 제목</label></td>
-			      			<td><input id="textTitle" type="text" name="blogTitle" value=""></td>
+			      			<td><input id="textTitle" type="text" name="blogTitle" value="${blogVo.blogTitle }"></td>
 			      		</tr>
 			      		<tr>
 			      			<td><label>로고이미지</label></td>
@@ -56,7 +58,7 @@
 		
 		
 		<!-- 개인블로그 푸터 -->
-		
+		<c:import url="/WEB-INF/views/includes/blog-footer.jsp"></c:import>
 	
 	
 	</div>
